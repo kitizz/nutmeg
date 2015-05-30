d116b8c56a6c57c60c29ced744175ded448a2941
Kit Ham <kitizz.devside@gmail.com>
2015-05-30 12:21:13 -0400

    Even better code

--- python/examples/basic.py
+++ python/examples/basic.py
@@ -1,12 +1,12 @@
 import Nutmeg
-from numpy import sin, cos, pi
+from numpy import sin, cos, pi, r_
 
 # Assuming the core is on port 43686 (default)
 Nutmeg.init()
 
-x = [ 0.01*n for n in range(100) ]
-y1 = [ sin(10*pi*t) for t in x ]
-y2 = [ 10*pi*cos(10*pi*t) for t in x ]
+x = r_[0:1:0.01]
+y1 = sin(10*pi*x)
+y2 = 10*pi*cos(10*pi*x)
 
 fig = Nutmeg.figure("myFigure", "myFigure.qml")
 
