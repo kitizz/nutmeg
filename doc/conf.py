d00786c0c2922897c02b17dde52b4bae586d2398
Kit Ham <kitizz.devside@gmail.com>
2015-05-30 18:13:14 -0400

    Experimenting with alternative themes....

--- doc/conf.py
+++ doc/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -19,6 +20,7 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 
 sys.path.append( os.path.abspath("../breathe/") )
+sys.path.append( os.path.abspath("../pygments-style-github/") )
 
 # Build the doxygen xml before building with Sphinx
 # import subprocess
@@ -33,7 +35,7 @@ sys.path.append( os.path.abspath("../breathe/") )
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'breathe']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'breathe', 'pygments-style-github']
 
 # Breathe (C++) projects
 breathe_projects = { "Nutmeg": os.path.abspath("./xml/") }
@@ -94,7 +96,7 @@ exclude_patterns = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'github'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -104,7 +106,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -112,7 +114,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
